61caafb4a9bd4b7d734d4498a5be06019245744b
tourank <touranm@gmail.com>
2020-06-03 01:28:22 -0400

    heroku test

--- application.py
+++ application.py
@@ -1,5 +1,5 @@
 import os
-from flask import Flask, session, render_template
+from flask import Flask, session, render_template, request 
 from flask_session import Session
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
